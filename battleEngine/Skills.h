657bb29aa45fce0bbe1f22bc303707b7852ea929
ew654 <e.wang.654@gmail.com>
2013-12-14 22:39:09 -0500

    meow

--- battleEngine/Skills.h
+++ battleEngine/Skills.h
@@ -7,7 +7,60 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Unit.h"
 
 @interface Skills : NSObject
 
+@property NSUInteger magicPower; // level 1 60 /  level 5 80
+@property NSUInteger attackPower; // level 1 40 / level 5 60
+@property NSUInteger magicDefense; // level 1 50 / level 5 75
+@property NSUInteger defensePower; // level 1 60 / level 5 80
+@property NSUInteger healthPoints; // level 1 250 / level 5 550
+@property NSUInteger manaPoints; // level 1 100 / level 5 200
+@property NSUInteger level;
+
+-(void)magicMissle:(Unit *)f;
+-(void)fireball:(Unit *)f;
+
+
+-(void)disarm:(Unit *)f;
+-(void)bullRush:(Unit *)f;
+
+-(void)dirtyTrick:(Unit *)f;
+-(void)backstab:(Unit *)f;
+
+
 @end
+
+// example Fireball does x base damage that is then modified by magic attack from the Unit class
+// healthPoints max per level:
+// healthPoints max per level:
+// attackPower max per level:
+// magicPower max per level:
+// defensePower max per level:
+// magicDefense max per level:
+
+
+/* 
+
+-(void)attackPlayer:(Character *)f;
+{
+    int critChance = arc4random()%99+1;
+    if (critChance <= 25) {
+        if (self.attackPower >30) {
+            NSLog(@"Critical hard!");
+            if (f.defensePower >= self.attackPower) {
+                f.healthPoints = f.healthPoints-(1*critChance);
+                NSLog(@"Defense is too great");
+            } else {
+                if (self.attackPower >30) {
+                    int atkMod = arc4random()%10+1;
+                    f.healthPoints= (f.healthPoints +f.defensePower)-(self.attackPower*atkMod);
+                    NSLog(@"Crit damage for %i", (self.attackPower*atkMod)-f.defensePower);
+                } else {
+                    int atkMod2 = arc4random()%5+1;
+                    f.healthPoints = (f.healthPoints +f.defensePower)-(self.attackPower*atkMod2);
+                    NSLog(@"Crit damage for %i", (self.attackPower*atkMod2)-f.defensePower);
+                }
+            }
+*/
